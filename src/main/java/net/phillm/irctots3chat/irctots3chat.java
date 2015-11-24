42538db891f4c639d83829ea8a0bfe725b4f66b0
Phillip <Phillip@192.168.1.50>
2015-11-24 21:22:20 +1100

    add skype command test

--- src/main/java/net/phillm/irctots3chat/irctots3chat.java
+++ src/main/java/net/phillm/irctots3chat/irctots3chat.java
@@ -5,10 +5,12 @@
  */
 package net.phillm.irctots3chat;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -80,6 +82,29 @@ public class irctots3chat {
                
 
 }
+        public static String executeCommand(String command) {
+
+		StringBuffer output = new StringBuffer();
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader = 
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+                        String line = "";			
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return output.toString();
+
+	}
         
 }
  
