35690bbe66bc4a56e57e05d07d2aef0d1025fe23
Phillip <Phillip@192.168.1.50>
2015-12-08 12:41:27 +1100

    changed ircConfigMap to be public property

--- src/main/java/net/phillm/irctots3chat/irctots3chat.java
+++ src/main/java/net/phillm/irctots3chat/irctots3chat.java
@@ -24,87 +24,85 @@ import org.yaml.snakeyaml.Yaml;
  * @author Phillip
  */
 public class irctots3chat {
+
     private static TS3Bot ts3;
     private static MultiBotManager<PircBotX> ircbotmanager;
-                 
+    public static Map<String, String> ircConfigMap = null;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
             //new ChatBotExample();
-            //new TS3Bot();
-            try {
-                System.out.println("Chatbridge initalizing");
+        //new TS3Bot();
+        try {
+            System.out.println("Chatbridge initalizing");
                                 //Configure what we want our bot to do
-                
-                 InputStream ircConfigInput = null;
-                try {
-                    ircConfigInput =  new FileInputStream(new File("ircconfig.yml"));
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    System.out.println("Could not find ircconfig.yml. Please check that it exists.");
-                }
 
-                Yaml ircConfigParser = new Yaml();
-                Map<String,String> ircConfigMap = (Map<String,String>)ircConfigParser.load(ircConfigInput);
-                
-                Configuration configuration = new Configuration.Builder()
-                        .setName(ircConfigMap.get("nick")) //Set the nick of the bot.
-                        .setServerHostname(ircConfigMap.get("host")) //
-                        .addAutoJoinChannel(ircConfigMap.get("channel"), "") //
-                        .addListener(new IRCListener()) //Add our listener that will be called on Events
-                        .buildConfiguration();
-
-               //Create our bot with the configuration
-                MultiBotManager<PircBotX> manager = new MultiBotManager();
-                manager.addBot(configuration);
-                //PircBotX bot = new PircBotX(configuration);
-                //Connect to the server
-                //bot.startBot();
-                manager.start();
-                ircbotmanager = manager;
-                
-               ts3 =  new TS3Bot();
-                
-                
-            } catch (Exception ex) {
-                Logger.getLogger(irctots3chat.class.getName()).log(Level.SEVERE, null, ex);
+            InputStream ircConfigInput = null;
+            try {
+                ircConfigInput = new FileInputStream(new File("ircconfig.yml"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("Could not find ircconfig.yml. Please check that it exists.");
             }
-            
-            
-	}
-        public static TS3Bot getTS3(){
-            return ts3;
-            
-        
+
+            Yaml ircConfigParser = new Yaml();
+            ircConfigMap = (Map<String, String>) ircConfigParser.load(ircConfigInput);
+
+            Configuration configuration = new Configuration.Builder()
+                    .setName(ircConfigMap.get("nick")) //Set the nick of the bot.
+                    .setServerHostname(ircConfigMap.get("host")) //
+                    .addAutoJoinChannel(ircConfigMap.get("channel"), "") //
+                    .addListener(new IRCListener()) //Add our listener that will be called on Events
+                    .buildConfiguration();
+
+            //Create our bot with the configuration
+            MultiBotManager<PircBotX> manager = new MultiBotManager();
+            manager.addBot(configuration);
+                //PircBotX bot = new PircBotX(configuration);
+            //Connect to the server
+            //bot.startBot();
+            manager.start();
+            ircbotmanager = manager;
+
+            ts3 = new TS3Bot();
+
+        } catch (Exception ex) {
+            Logger.getLogger(irctots3chat.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        public static MultiBotManager<PircBotX> getIRCManger () {
+
+    }
+
+    public static TS3Bot getTS3() {
+        return ts3;
+
+    }
+
+    public static MultiBotManager<PircBotX> getIRCManger() {
         return ircbotmanager;
-               
+
+    }
+
+    public static String executeCommand(String[] command) {
+
+        StringBuilder output = new StringBuilder();
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader
+                    = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+
+    }
 
 }
-        public static String executeCommand(String[] command) {
-
-		StringBuilder output = new StringBuilder();
-
-		Process p;
-		try {
-			p = Runtime.getRuntime().exec(command);
-			p.waitFor();
-			BufferedReader reader = 
-                            new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-                        String line = "";			
-			while ((line = reader.readLine())!= null) {
-				output.append(line + "\n");
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return output.toString();
-
-	}
-        
-}
- 
