b35ce7fe8aec9738d4eec125372c698b1432eb2a
Phillip <Phillip@192.168.1.50>
2015-12-15 19:39:48 +1100

    debug ts3 onClientJoin event

--- src/main/java/net/phillm/irctots3chat/TS3Bot.java
+++ src/main/java/net/phillm/irctots3chat/TS3Bot.java
@@ -163,35 +163,35 @@ public class TS3Bot {
                 
                 System.out.println("new client ID: " + joiningClientId.toString());
                 
-                //try {
-                //    Thread.sleep(100);
-                //} catch (InterruptedException ex) {
-                //    Logger.getLogger(TS3Bot.class.getName()).log(Level.SEVERE, null, ex);
-                //}
-               
-                List<Client> clientList = api.getClients();
-                List<Integer> clientIdsList = api.getClients.map(#Client.getId());
-                
-                if (clientIdsList.contains(joiningClientId)) {
-
-                    joiningClient = api.getClientInfo(joiningClientId);
-                    localInfo = api.whoAmI();
-                    channelInfo = api.getChannelInfo(localInfo.getChannelId());
-                    String originalClientName = joiningClient.getNickname();
-                    String clientName = nameTagStrip(originalClientName);
-
-                    if ((joiningClient.getChannelId() == localInfo.getChannelId()) && (!localInfo.getNickname().equals(originalClientName))) {
-                        OutputChannel ircchannel;
-
-                        ircchannel = irctots3chat.getIRCManger().getBots().first().getUserChannelDao().getChannel(irctots3chat.ircConfigMap.get("channel")).send();
-                        ircchannel.message(clientName + " Joined Channel " + channelInfo.getName());
-                        uidsInChannel.put(joiningClientId, clientName);
-                    } else {
-                        System.out.println("onClientJoin fired: Unrelated client");
-                    }
-                } else {
-                    System.out.println("New client with ID: " + joiningClientId + "left");
-                }
+//                try {
+//                    Thread.sleep(100);
+//                } catch (InterruptedException ex) {
+//                    Logger.getLogger(TS3Bot.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//               
+//                List<Client> clientList = api.getClients();
+//                List<Integer> clientIdsList = api.getClients.map(#Client.getId());
+//                
+//                if (clientIdsList.contains(joiningClientId)) {
+//
+//                    joiningClient = api.getClientInfo(joiningClientId);
+//                    localInfo = api.whoAmI();
+//                    channelInfo = api.getChannelInfo(localInfo.getChannelId());
+//                    /String originalClientName = joiningClient.getNickname();
+//                    String clientName = nameTagStrip(originalClientName);
+//
+//                    if ((joiningClient.getChannelId() == localInfo.getChannelId()) && (!localInfo.getNickname().equals(originalClientName))) {
+//                        OutputChannel ircchannel;
+//
+//                        ircchannel = irctots3chat.getIRCManger().getBots().first().getUserChannelDao().getChannel(irctots3chat.ircConfigMap.get("channel")).send();
+//                        ircchannel.message(clientName + " Joined Channel " + channelInfo.getName());
+//                        uidsInChannel.put(joiningClientId, clientName);
+//                    } else {
+//                        System.out.println("onClientJoin fired: Unrelated client");
+//                    }
+//                } else {
+//                    System.out.println("New client with ID: " + joiningClientId + "left");
+//                }
             }
 
             @Override
