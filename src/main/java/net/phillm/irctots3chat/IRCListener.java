98cd7e04568bd477c0030ec57e9443bb0a200678
Phillip <Phillip@192.168.1.50>
2015-12-15 15:10:16 +1100

    fix skype-msg command

--- src/main/java/net/phillm/irctots3chat/IRCListener.java
+++ src/main/java/net/phillm/irctots3chat/IRCListener.java
@@ -39,12 +39,12 @@ public class IRCListener extends ListenerAdapter {
             if (nickname.equalsIgnoreCase("skype")) {
                 System.out.println("nick " + nickname + " is skype. not sending msg");
             } else {
-                String sendToSkypeResult = irctots3chat.executeCommand(new String[]{"if [ -x ./skype-msg.sh]; then ./skype-msg.sh ", nickname, ": ", message, "; fi"}).trim();
+                String sendToSkypeResult = irctots3chat.executeCommand(new String[]{"if [ -x ./skype-msg.sh]; then ./skype-msg.sh ", nickname + ": '" + message + "'; fi"}).trim();
                 System.out.println("Got restult: " + sendToSkypeResult);
                 if (sendToSkypeResult.equalsIgnoreCase("OK")) {
-                    System.out.println("sent to skype: ./skype-msg.sh " + "'" + nickname + ":" + message + "'");
+                    System.out.println("sent to skype: ./skype-msg.sh " + "'" + nickname + ": " + message + "'");
                 } else {
-                    System.out.println("Failed to send to skype: ./skype-msg.sh " + "'" + nickname + ":" + message + "'");
+                    System.out.println("Failed to send to skype: ./skype-msg.sh " + "'" + nickname + ": " + message + "'");
                 }
             }
         }
